a46a22458265186f7a866427b7be4803df552284
timdiels <timdiels.m@gmail.com>
2017-01-11 13:12:15 +0100

    Optimization: TopK debug logging was accidentally reenabled
    Reenable happened when TopK was moved out of soylent_recipes.various

--- soylent_recipes/main.py
+++ soylent_recipes/main.py
@@ -42,7 +42,7 @@ def main(usda_directory, output_clustering, miner_algorithm):
     colored_traceback.add_hook()
     logging_.configure('soylent.log')
     logging.getLogger().setLevel(logging.DEBUG)
-    logging.getLogger('soylent_recipes.various').setLevel(logging.INFO)
+    logging.getLogger('soylent_recipes.mining.top_k').setLevel(logging.INFO)
     _logger.info('Hi!!! This is version none!')
     nutrition_target = nutrition_target_.from_config()
     foods = foods_.import_usda(Path(usda_directory))
